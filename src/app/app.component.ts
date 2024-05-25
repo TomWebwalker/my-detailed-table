3b352ad10cee101639811c0cd4d75a0d73e1f32f
Tomasz Flis <tomaszwwl@gmail.com>
2024-05-25 17:13:41 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import {MatTableModule} from "@angular/material/table";
+import {MatAnchor} from "@angular/material/button";
+import {PeriodicDataService} from "./periodic-data.service";
+import {MatDrawer, MatDrawerContainer} from "@angular/material/sidenav";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTableModule, MatAnchor, RouterLink, MatDrawerContainer, MatDrawer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private readonly router = inject(Router);
+  readonly periodicData = inject(PeriodicDataService).getData();
   title = 'my-detailed-table';
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'details'];
+  sideBarOpen = false;
+
+  drawerClosed(): void {
+    this.router.navigate(['']);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {PeriodicDetailsComponent} from "./periodic-details/periodic-details.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: ':id',
+    component: PeriodicDetailsComponent,
+    outlet: 'side-bar'
+  }
+];
